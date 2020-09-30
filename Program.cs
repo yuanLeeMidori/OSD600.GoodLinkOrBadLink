138f7da43dfce3f5486a446cd38b9f4b1b15d817
rogercyyu <roger.yu27@gmail.com>
2020-09-30 13:43:27 -0400

    finished wayback feature

--- Program.cs
+++ Program.cs
@@ -24,114 +24,125 @@ namespace OSD600.GoodLinkOrBadLink
                 return;
 
             }else{
-                // TODO: need to add case for wayback WayBack
+
                 bool readArgAsFile = CLIUsage.Version(args[0]);
                 bool wayback = CLIUsage.WayBack(args[0]);
+                
                 if(readArgAsFile){
 
                 }else{
             
 
                 
-                    Console.WriteLine("Is this working??");
                     try{
-                        string filePath;
-                        if(wayback) {
-                            Console.WriteLine(wayback);
-                            Console.WriteLine(args[0]);
-                            Console.WriteLine(args[1]);
-                            filePath = args[1];
-                        } else {
-
-                            filePath = args[0];
-
-                        }
-                
-                        string[] fileContent = File.ReadAllLines(filePath);
+                        string filePath = args[0];
 
 
-                        if(fileContent == null || fileContent.Length < 1){
-                            
-                            Console.WriteLine("\"{0}\" is empty, there is nothing to test", filePath);
+                            if(wayback) {
+                                try {
+                                    filePath = args[1];
+                                } catch(Exception) {
+                                    Console.WriteLine("Missing file input.");
+
+                                    System.Environment.Exit(1);
+                                }
+                            }
+                            string[] fileContent = File.ReadAllLines(filePath);
                         
-                        }else{
 
-                
-                                        Regex rx = new Regex(@"https?://[a-zA-Z0-9@:%._\+~#=]");
+                            if(fileContent == null || fileContent.Length < 1){
+                                
+                                Console.WriteLine("\"{0}\" is empty, there is nothing to test", filePath);
+                            
+                            }else{
 
-                                        
-                                        string[] urls = File.ReadAllLines(filePath);
+        
+                                Regex rx = new Regex(@"https?://[a-zA-Z0-9@:%._\+~#=]");
+
+                                
+                                string[] urls = File.ReadAllLines(filePath);
+
+                            
+                    
+
+                                List<string> lines = new List<string>();
+                            
+
+
+                                foreach(String line in urls){
 
                                     
-                            
-
-                                        List<string> lines = new List<string>();
                                     
 
+                                    if(rx.IsMatch(line)){
+                
+                                        try{
 
-                                        foreach(String line in urls){
+                                            if (wayback) {
 
-                                            
-                                            
+                                                var jsonString = new WebClient().DownloadString("http://archive.org/wayback/available?url=" + line);
+                                                dynamic x = Newtonsoft.Json.JsonConvert.DeserializeObject(jsonString);
+                                                var archived = x.archived_snapshots;
+                                                dynamic available;
+                                                try {
+                                                    var closest = archived.closest;
+                                                    available = closest.available;
+                                                }catch(Exception){
+                                                    available = false;
+                                                }
 
-                                            if(rx.IsMatch(line)){
-                        
-                                                try{
-
-                                                    if (wayback) {
-                                                        // HttpResponseMessage response = await client.GetAsync("http://archive.org/wayback/available?url=" + line);
-                                                        var jsonString = new WebClient().DownloadString("http://archive.org/wayback/available?url=" + line);
-                                                        // Console.Write(json);
-                                                        dynamic x = Newtonsoft.Json.JsonConvert.DeserializeObject(jsonString);
-                                                        var archived = x.archived_snapshots;
-                                                        dynamic available;
-                                                        try {
-                                                            var closest = archived.closest;
-                                                            available = closest.available;
-                                                        }catch(NullReferenceException){
-                                                            available = false;
-                                                        }
-                                                        Console.WriteLine((bool)available);
-  
-                                                    } else {
-                                                        HttpResponseMessage response = await client.GetAsync(line);
-                                                        if((int)response.StatusCode == 200){
-
-                                                            Console.ForegroundColor = ConsoleColor.Green;
-                                                            Console.Write("[Good] ");
-                                                            Console.ResetColor();
-                                                            Console.WriteLine(line);
-
-                                                        }else if((int)response.StatusCode == 400 || (int)response.StatusCode == 404){
-                                                            
-                                                            Console.ForegroundColor = ConsoleColor.Red;
-                                                            Console.Write("[Bad] ");
-                                                            Console.ResetColor();
-                                                            Console.WriteLine(line);
-                                                        }
-                                                    }
-
-                                                
+                                                if ((bool)available) {
+                                                    Console.ForegroundColor = ConsoleColor.Green;
+                                                    Console.Write("[Good] ");
+                                                    Console.ResetColor();
+                                                    Console.WriteLine(line);
+                                                } else {
+                                                    Console.ForegroundColor = ConsoleColor.Red;
+                                                    Console.Write("[Bad] ");
+                                                    Console.ResetColor();
+                                                    Console.WriteLine(line);
+                                                }   
 
 
-                                                }catch(HttpRequestException){
+                                            } else {
+                                                HttpResponseMessage response = await client.GetAsync(line);
+                                                if((int)response.StatusCode == 200){
 
-                                                    Console.ForegroundColor = ConsoleColor.Gray;
-                                                    Console.Write("[Unknown] ");
+                                                    Console.ForegroundColor = ConsoleColor.Green;
+                                                    Console.Write("[Good] ");
                                                     Console.ResetColor();
                                                     Console.WriteLine(line);
 
+                                                }else if((int)response.StatusCode == 400 || (int)response.StatusCode == 404){
                                                     
-
+                                                    Console.ForegroundColor = ConsoleColor.Red;
+                                                    Console.Write("[Bad] ");
+                                                    Console.ResetColor();
+                                                    Console.WriteLine(line);
                                                 }
-
-                                            }else{
-
-                                                Console.WriteLine("not a URL");
                                             }
 
+                                        
+
+
+                                        }catch(HttpRequestException){
+
+                                            Console.ForegroundColor = ConsoleColor.Gray;
+                                            Console.Write("[Unknown] ");
+                                            Console.ResetColor();
+                                            Console.WriteLine(line);
+
+                                            
+
                                         }
-                        } 
+
+                                    }else{
+
+                                        Console.WriteLine("not a URL");
+                                    }
+
+                                }
+                } 
 
                     }catch(FileNotFoundException e){
 
